ea9d938773a9a4cae5d35f6aba494719ffbaa52f
Abhishek <abhishekshukla821@gmail.com>
2024-07-06 22:53:26 +0530

    style: add footer to aside compo.

--- src/components/Containers/Aside.jsx
+++ src/components/Containers/Aside.jsx
@@ -1,13 +1,22 @@
-import React from "react";
 import { Search, WhatsHappening, WhoToFollow } from "../index";
-import { Outlet } from "react-router-dom";
 
 function Aside() {
     return (
-        <aside className="max-w-xl sticky top-0 mr-auto overflow-y-auto overflow-x-hidden min-h-full hidden min-[1005px]:block">
+        <aside className="max-w-xl sticky top-0 mr-auto overflow-y-auto overflow-x-hidden min-h-full hidden lg:block">
             <Search />
             <WhatsHappening />
             <WhoToFollow />
+            <div className="p-5 text-zinc-600 text-[13px] max-w-[22rem]">
+                <ul className="flex gap-2 flex-wrap">
+                    <li>Terms of Service</li>
+                    <li>Privacy Policy</li>
+                    <li>Cookie Policy</li>
+                    <li>Accessibility</li>
+                    <li>Ads info</li>
+                    <li>More...</li>
+                    <li>&copy; 2024 X Corp.</li>
+                </ul>
+            </div>
         </aside>
     );
 }
--- src/components/WhoToFollow/WhoToFollow.jsx
+++ src/components/WhoToFollow/WhoToFollow.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import FollowCard from "./FollowCard";
 
 function WhoToFollow() {
