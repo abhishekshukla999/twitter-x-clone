da922c24d79d7b336e733fdef33cc3d42c7de36c
Abhishek <abhishekshukla821@gmail.com>
2024-08-09 12:04:58 +0530

    fix: load logged profile likes to current profile likes

--- src/components/Profile/Likes.jsx
+++ src/components/Profile/Likes.jsx
@@ -7,67 +7,59 @@ import { addLikes } from "../../features/like/likeSlice";
 
 function Likes() {
     const otherProfileData = useSelector((state) => state.otherProfile);
-    const authData = useSelector((state) => state.auth.userData);
     const likesData = useSelector((state) => state.likes);
-    const [loader, setLoader] = useState(true);
+    const [loading, setLoading] = useState(true);
     const dispatch = useDispatch();
 
     useEffect(() => {
         async function fetchLikes() {
             try {
-                if (authData) {
-                    const myLikes = await likeService.getLikes([
-                        Query.equal("userId", [authData.$id]),
-                        Query.orderDesc("$createdAt"),
+                const myLikes = await likeService.getLikes([
+                    Query.equal("userId", [otherProfileData.$id]),
+                ]);
+
+                if (myLikes.documents.length !== 0) {
+                    const tweetIds = myLikes.documents.map(
+                        (like) => like.tweetId
+                    );
+
+                    const likedTweets = await tweetService.getTweets([
+                        Query.equal("$id", tweetIds),
                     ]);
 
-                    if (myLikes.documents.length !== 0) {
-                        const tweetIds = myLikes.documents.map(
-                            (like) => like.tweetId
+                    // map of tweetId to like createdAt
+                    const likeMap = myLikes.documents.reduce((map, like) => {
+                        map[like.tweetId] = like.$createdAt;
+                        return map;
+                    }, {});
+
+                    // Sorting the tweets based on like creation date
+                    likedTweets.documents.sort((a, b) => {
+                        return (
+                            new Date(likeMap[b.$id]) - new Date(likeMap[a.$id])
                         );
+                    });
 
-                        const likedTweets = await tweetService.getTweets([
-                            Query.equal("$id", tweetIds),
-                        ]);
-
-                        // map of tweetId to like createdAt
-                        const likeMap = myLikes.documents.reduce(
-                            (map, like) => {
-                                map[like.tweetId] = like.$createdAt;
-                                return map;
-                            },
-                            {}
-                        );
-
-                        // Sorting the tweets based on like creation date
-                        likedTweets.documents.sort((a, b) => {
-                            return (
-                                new Date(likeMap[b.$id]) -
-                                new Date(likeMap[a.$id])
-                            );
-                        });
-
-                        dispatch(
-                            addLikes({
-                                data: likedTweets.documents,
-                                likesCount: myLikes.documents.length,
-                            })
-                        );
-                    }
+                    dispatch(
+                        addLikes({
+                            data: likedTweets.documents,
+                            likesCount: myLikes.documents.length,
+                        })
+                    );
                 }
             } catch (error) {
                 console.error("Error in fetching bookmarks :: ", error);
             } finally {
-                setLoader(false);
+                setLoading(false);
             }
         }
 
         fetchLikes();
-    }, [dispatch, likesData.likesCount]);
+    }, [dispatch, likesData.likesCount, otherProfileData.$id]);
 
     return (
         <div>
-            {loader ? (
+            {loading ? (
                 <Loader />
             ) : likesData.likesCount === 0 ? (
                 <div className="p-4 text-2xl font-bold text-center border-l border-r">
