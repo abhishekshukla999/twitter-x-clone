60acad0d0d8c4de73d765e948b5937755ae515a7
Abhishek <abhishekshukla821@gmail.com>
2024-08-13 16:37:24 +0530

    refactor: add loader to phone and email change

--- src/components/Settings/YourAccount/AccountInformation/PhoneChange.jsx
+++ src/components/Settings/YourAccount/AccountInformation/PhoneChange.jsx
@@ -1,10 +1,11 @@
 import { NavLink, useNavigate } from "react-router-dom";
-import { Input } from "../../../index";
+import { Input, LoadingModal } from "../../../index";
 import { useDispatch, useSelector } from "react-redux";
 import { useForm } from "react-hook-form";
 import { profileService } from "../../../../appwrite";
 import { addProfileData } from "../../../../features/profile/profileSlice";
 import { toast } from "sonner";
+import { useState } from "react";
 
 function PhoneChange() {
     const profileData = useSelector((state) => state.profile);
@@ -18,10 +19,13 @@ function PhoneChange() {
     } = useForm({
         defaultValues: { phone: profileData?.phone || "" },
     });
+    const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
     const dispatch = useDispatch();
 
     const changePhone = async (data, action) => {
+        setLoading(true);
+
         if (data.phone === profileData?.phone && action === "update") return;
 
         if (authData) {
@@ -46,6 +50,7 @@ function PhoneChange() {
                 toast.error("Phone number updating failed");
             } finally {
                 reset({ phone: data?.phone });
+                setLoading(false);
             }
         }
     };
@@ -59,66 +64,75 @@ function PhoneChange() {
     };
 
     return (
-        <div className="xl:flex-[0_0_43%] border-r h-full sticky top-0 overflow-y-auto">
-            <div className="top flex sticky top-0 backdrop-blur-3xl opacity-[100%]">
-                <div className="flex gap-5">
-                    <NavLink
-                        className="m-0.5 my-auto p-2 hover:bg-gray-200 rounded-full"
-                        onClick={() => navigate(-1)}
-                    >
-                        <svg
-                            viewBox="0 0 24 24"
-                            aria-hidden="true"
-                            className="w-5 r-4qtqp9 r-yyyyoo r-dnmrzs r-bnwqim r-lrvibr r-m6rgpd r-z80fyv r-19wmn03"
+        <>
+            <div className="xl:flex-[0_0_43%] border-r h-full sticky top-0 overflow-y-auto">
+                <div className="top flex sticky top-0 backdrop-blur-3xl opacity-[100%]">
+                    <div className="flex gap-5">
+                        <NavLink
+                            className="m-0.5 my-auto p-2 hover:bg-gray-200 rounded-full"
+                            onClick={() => navigate(-1)}
                         >
-                            <g>
-                                <path d="M7.414 13l5.043 5.04-1.414 1.42L3.586 12l7.457-7.46 1.414 1.42L7.414 11H21v2H7.414z"></path>
-                            </g>
-                        </svg>
-                    </NavLink>
-                    <div className="font-bold text-xl py-3">Change phone</div>
+                            <svg
+                                viewBox="0 0 24 24"
+                                aria-hidden="true"
+                                className="w-5 r-4qtqp9 r-yyyyoo r-dnmrzs r-bnwqim r-lrvibr r-m6rgpd r-z80fyv r-19wmn03"
+                            >
+                                <g>
+                                    <path d="M7.414 13l5.043 5.04-1.414 1.42L3.586 12l7.457-7.46 1.414 1.42L7.414 11H21v2H7.414z"></path>
+                                </g>
+                            </svg>
+                        </NavLink>
+                        <div className="font-bold text-xl py-3">
+                            Change phone
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <form onSubmit={handleSubmit(() => {})}>
-                <div className="border-b py-4 px-3">
-                    <Input
-                        label="Current"
-                        type="text"
-                        inputMode="numeric"
-                        placeholder="Type phone number"
-                        {...register("phone", {
-                            required: true,
-                            pattern: /^[0-9]*$/,
-                        })}
-                        onInput={(e) =>
-                            (e.target.value = e.target.value.replace(/\D/g, ""))
-                        }
-                    />
+                <form onSubmit={handleSubmit(() => {})}>
+                    <div className="border-b py-4 px-3">
+                        <Input
+                            label="Current"
+                            type="text"
+                            inputMode="numeric"
+                            placeholder="Type phone number"
+                            {...register("phone", {
+                                required: true,
+                                pattern: /^[0-9]*$/,
+                            })}
+                            onInput={(e) =>
+                                (e.target.value = e.target.value.replace(
+                                    /\D/g,
+                                    ""
+                                ))
+                            }
+                        />
 
-                    {!isValid && (
-                        <p className="text-red-500">
-                            Please write a valid phone number
-                        </p>
-                    )}
-                </div>
-                <div className="my-1 box-border">
-                    <button
-                        type="button"
-                        onClick={handleUpdate}
-                        className="py-3 px-4 text-center w-full text-twitter-blue hover:bg-blue-100"
-                    >
-                        Update phone number
-                    </button>
-                    <button
-                        type="button"
-                        onClick={handleDelete}
-                        className="py-3 px-4 text-center w-full text-red-500 hover:bg-red-100"
-                    >
-                        Delete phone number
-                    </button>
-                </div>
-            </form>
-        </div>
+                        {!isValid && (
+                            <p className="text-red-500">
+                                Please write a valid phone number
+                            </p>
+                        )}
+                    </div>
+                    <div className="my-1 box-border">
+                        <button
+                            type="button"
+                            onClick={handleUpdate}
+                            className="py-3 px-4 text-center w-full text-twitter-blue hover:bg-blue-100"
+                        >
+                            Update phone number
+                        </button>
+                        <button
+                            type="button"
+                            onClick={handleDelete}
+                            className="py-3 px-4 text-center w-full text-red-500 hover:bg-red-100"
+                        >
+                            Delete phone number
+                        </button>
+                    </div>
+                </form>
+            </div>
+
+            <LoadingModal isOpen={loading} />
+        </>
     );
 }
 
--- src/components/Settings/YourAccount/AccountInformation/UsernameChange.jsx
+++ src/components/Settings/YourAccount/AccountInformation/UsernameChange.jsx
@@ -1,5 +1,5 @@
 import { NavLink, useNavigate } from "react-router-dom";
-import { Input } from "../../../index";
+import { Input, LoadingModal } from "../../../index";
 import { useDispatch, useSelector } from "react-redux";
 import { useForm } from "react-hook-form";
 import { profileService } from "../../../../appwrite";
@@ -22,6 +22,7 @@ function UsernameChange() {
     });
     const [isSave, setIsSave] = useState(true);
     const [usernameAvailable, setUsernameAvailable] = useState(true);
+    const [loading, setLoading] = useState(false);
     const dispatch = useDispatch();
 
     const currentUsername = watch("username");
@@ -56,6 +57,8 @@ function UsernameChange() {
     }, [currentUsername, profileData?.username]);
 
     const changeUsername = async (data) => {
+        setLoading(true);
+
         if (authData) {
             try {
                 const updatedProfileData = await profileService.updateProfile(
@@ -72,78 +75,84 @@ function UsernameChange() {
                 toast.success("Username changed successfully !!");
             } catch (error) {
                 // console.log("Error updating username :: ", error);
-                toast.success("Error changing username !!");
+                toast.error("Error changing username !!");
+            } finally {
+                setLoading(false);
             }
         }
     };
 
     return (
-        <div className="xl:flex-[0_0_43%] border-r h-full sticky top-0 overflow-y-auto">
-            <div className="top flex sticky top-0 backdrop-blur-3xl opacity-[100%]">
-                <div className="flex gap-5">
-                    <NavLink
-                        className="m-0.5 my-auto p-2 hover:bg-gray-200 rounded-full"
-                        onClick={() => navigate(-1)}
-                    >
-                        <svg
-                            viewBox="0 0 24 24"
-                            aria-hidden="true"
-                            className="w-5 r-4qtqp9 r-yyyyoo r-dnmrzs r-bnwqim r-lrvibr r-m6rgpd r-z80fyv r-19wmn03"
+        <>
+            <div className="xl:flex-[0_0_43%] border-r h-full sticky top-0 overflow-y-auto">
+                <div className="top flex sticky top-0 backdrop-blur-3xl opacity-[100%]">
+                    <div className="flex gap-5">
+                        <NavLink
+                            className="m-0.5 my-auto p-2 hover:bg-gray-200 rounded-full"
+                            onClick={() => navigate(-1)}
                         >
-                            <g>
-                                <path d="M7.414 13l5.043 5.04-1.414 1.42L3.586 12l7.457-7.46 1.414 1.42L7.414 11H21v2H7.414z"></path>
-                            </g>
-                        </svg>
-                    </NavLink>
-                    <div className="font-bold text-xl py-3">
-                        Change username
+                            <svg
+                                viewBox="0 0 24 24"
+                                aria-hidden="true"
+                                className="w-5 r-4qtqp9 r-yyyyoo r-dnmrzs r-bnwqim r-lrvibr r-m6rgpd r-z80fyv r-19wmn03"
+                            >
+                                <g>
+                                    <path d="M7.414 13l5.043 5.04-1.414 1.42L3.586 12l7.457-7.46 1.414 1.42L7.414 11H21v2H7.414z"></path>
+                                </g>
+                            </svg>
+                        </NavLink>
+                        <div className="font-bold text-xl py-3">
+                            Change username
+                        </div>
                     </div>
                 </div>
-            </div>
-            <form onSubmit={handleSubmit(changeUsername)}>
-                <div className="border-b py-4 px-3">
-                    <Input
-                        label="Username"
-                        type="text"
-                        placeholder="Type username"
-                        {...register("username", {
-                            required: true,
-                        })}
-                    />
-                    {profileData?.username !== currentUsername &&
-                    currentUsername.length !== 0 ? (
-                        usernameAvailable ? (
-                            <p className="text-green-500">
-                                @{currentUsername} is available
-                            </p>
+                <form onSubmit={handleSubmit(changeUsername)}>
+                    <div className="border-b py-4 px-3">
+                        <Input
+                            label="Username"
+                            type="text"
+                            placeholder="Type username"
+                            {...register("username", {
+                                required: true,
+                            })}
+                        />
+                        {profileData?.username !== currentUsername &&
+                        currentUsername.length !== 0 ? (
+                            usernameAvailable ? (
+                                <small className="text-green-500">
+                                    @{currentUsername} is available
+                                </small>
+                            ) : (
+                                <small className="text-red-500">
+                                    @{currentUsername} is already taken
+                                </small>
+                            )
                         ) : (
-                            <p className="text-red-500">
-                                @{currentUsername} is already taken
-                            </p>
-                        )
-                    ) : (
-                        !isValid && (
-                            <p className="text-red-500">
-                                Please write username
-                            </p>
-                        )
-                    )}
-                </div>
-                <div className="flex justify-end px-2 py-3">
-                    <button
-                        type="submit"
-                        className={`py-1.5 px-4 text-white font-bold ${
-                            isSave
-                                ? "bg-twitter-blue hover:bg-blue-500"
-                                : "bg-blue-300"
-                        } rounded-full`}
-                        disabled={!isSave}
-                    >
-                        Save
-                    </button>
-                </div>
-            </form>
-        </div>
+                            !isValid && (
+                                <small className="text-red-500">
+                                    Please write username
+                                </small>
+                            )
+                        )}
+                    </div>
+                    <div className="flex justify-end px-2 py-3">
+                        <button
+                            type="submit"
+                            className={`py-1.5 px-4 text-white font-bold ${
+                                isSave
+                                    ? "bg-twitter-blue hover:bg-blue-500"
+                                    : "bg-blue-300"
+                            } rounded-full`}
+                            disabled={!isSave}
+                        >
+                            Save
+                        </button>
+                    </div>
+                </form>
+            </div>
+
+            <LoadingModal isOpen={loading} />
+        </>
     );
 }
 
