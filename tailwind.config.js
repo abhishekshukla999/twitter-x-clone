c6556a8b2d6df7af9a3d63d079f38ab6244bbc65
Abhishek <abhishekshukla821@gmail.com>
2024-08-14 04:19:54 +0530

    chores: add custom themes to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    darkMode: [
+        "selector",
+        ".dark",
+        ".dim",
+        ".yellow",
+        ".crimson",
+        ".purple",
+        ".orange",
+        ".green",
+    ],
     theme: {
         extend: {
             colors: {
                 "twitter-blue": "#1D9BF0",
+                "twitter-yellow": "#FFD400",
+                "twitter-crimson": "#F91880",
+                "twitter-purple": "#7856FF",
+                "twitter-orange": "#FF7A00",
+                "twitter-green": "#00BA7C",
             },
         },
     },
-    plugins: [],
+    plugins: [
+        function ({ addVariant }) {
+            addVariant("purple", ".purple &");
+            addVariant("yellow", ".yellow &");
+            addVariant("crimson", ".crimson &");
+            addVariant("orange", ".orange &");
+            addVariant("green", ".green &");
+        },
+    ],
 };
