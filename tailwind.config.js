e3a475afeee4c3fe85191cc51b8bfea702daaed7
Abhishek <abhishekshukla821@gmail.com>
2024-08-14 16:54:05 +0530

    chores: add contrast and related colors to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,16 +10,25 @@ export default {
         ".purple",
         ".orange",
         ".green",
+        ".contrast",
     ],
     theme: {
         extend: {
             colors: {
+                "twitter-dim-bg": "#15202B",
+                "twitter-lightsout-bg": "#000000",
                 "twitter-blue": "#1D9BF0",
                 "twitter-yellow": "#FFD400",
                 "twitter-crimson": "#F91880",
                 "twitter-purple": "#7856FF",
                 "twitter-orange": "#FF7A00",
                 "twitter-green": "#00BA7C",
+                "contrast-twitter-blue": "#003886",
+                "contrast-twitter-yellow": "#6F3E00",
+                "contrast-twitter-crimson": "#890A46",
+                "contrast-twitter-purple": "#5234B7",
+                "contrast-twitter-orange": "#FFFFFF",
+                "contrast-twitter-green": "#FFFFFF",
             },
         },
     },
@@ -30,6 +39,7 @@ export default {
             addVariant("crimson", ".crimson &");
             addVariant("orange", ".orange &");
             addVariant("green", ".green &");
+            addVariant("contrast", ".contrast &");
         },
     ],
 };
