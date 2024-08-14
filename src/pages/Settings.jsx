d57aa2bf3c2b3c3b83000e625b19c1729af77979
Abhishek <abhishekshukla821@gmail.com>
2024-08-14 16:26:46 +0530

    refactor: add navigation on settings initial load

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,12 +1,20 @@
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import { SettingsList, NavigationBottom } from "../components";
 import { useMediaQuery } from "react-responsive";
+import { useEffect } from "react";
 
 function Settings() {
     const isWideScreen = useMediaQuery({ minWidth: "1005px" });
     const location = useLocation();
+    const navigate = useNavigate();
 
     const isBaseSettingsRoute = location.pathname === "/settings";
+
+    useEffect(() => {
+        if (isWideScreen) {
+            navigate("/settings/account");
+        }
+    }, []);
 
     return (
         <>
